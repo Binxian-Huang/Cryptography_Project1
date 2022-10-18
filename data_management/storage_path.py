deaf0f25d247bb0f9591c28448500b7e809d4b3e
Binxian-Huang <100451010@alumnos.uc3m.es>
2022-10-18 20:20:59 +0200

    Configuración inicial

---
+++ data_management/storage_path.py
@@ -0,0 +1,7 @@
+
+"""Global constants for finding the path"""
+from pathlib import Path
+JSON_FILES_PATH = str(Path.home()) + "/Uc3m/Curso3/Criptografia/Practica1/data_management/data_json/"
+
+#Ruta Álvaro:"/PycharmProjects/G82.2022.T06.PF/src/JsonFiles/"
+#Ruta Binxian:"/Uc3m/Curso3/Criptografia/Practica1/data_management/data_json/"
